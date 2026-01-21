bade2947f9f93525162af859726aeb9c6fac4519
wygol <rene.strammer@gmx.at>
2026-01-21 20:39:44 +0000

    docker compose file and individual docker run scripts

--- pipeline/dbContainerGeneration.sh
+++ pipeline/dbContainerGeneration.sh
@@ -12,4 +12,29 @@ sudo docker run -it \
   -e POSTGRES_DB="ny_taxi" \
   -v $(pwd)/ny_taxi_postgres_data:/var/lib/postgresql/ \
   -p 5432:5432 \
+  --network=pg-network\
+  --name=pgdatabase
   postgres:18
+# bring the database with the same blind mount to the network
+# and then the script and pgadmin can also use the network if needed to connect to the db
+
+# IMPORTANT: a container needs a name on a network so that it can be seen by other containers
+
+# ingestion container 
+# on the pg-network network so that it can connect to the correct localhost
+sudo docker run -it --rm  --name=ingestion --network=pipeline_default \ 
+zoomcamp1:v2 --user="root" \ 
+--password="root" --host="pgdatabase" \ 
+--port=5432 --db="ny_taxi" \ --table="yellow_taxi_data" 
+
+ # the containers dont have to be build again. instead they just live now in a network
+ # the dockerfile/image is still correct, it is just that the living environment has changed
+ # and the living environment is defined in the docker run
+ # host has to be adapted to the db containers name on the network (pgdatabase here)
+
+
+ # make a new container for pgadmin
+
+sudo docker run -it --rm -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
+ -e PGADMIN_DEFAULT_PASSWORD="root" -v $(pwd)/pgadmin_data:/var/lib/pgadmin \
+ -p 8085:80 --network=pg-network --name=pgadmin dpage/pgadmin4
